a37a18c3b35e6a972dca4afba3289b780fbd8f7d
ROCHESTER\JPhillips <jphillips@gleason.com>
2017-12-14 14:37:57 -0500

    added remove and update functions added an actiuon sheet. similiar to snack bar but able to add more functions to it

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
-import { AngularFireDatabase, AngularFireList, AngularFireAction, DatabaseSnapshot } from 'angularfire2/database';
+import { 
+  NavController, 
+  AlertController,
+  ActionSheetController } from 'ionic-angular';
+import { 
+  AngularFireDatabase, 
+  AngularFireList, 
+  AngularFireAction, 
+  DatabaseSnapshot } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -15,7 +22,8 @@ export class HomePage {
 
   constructor(public navCtrl: NavController,
     public afDatabase: AngularFireDatabase,
-    public alertCtrl: AlertController) {
+    public alertCtrl: AlertController,
+    public actionSheetCtrl: ActionSheetController) {
 
     //afDatabase.list('/songs').push('new song');
     afDatabase.list('/songs').valueChanges().subscribe(console.log);
@@ -52,6 +60,68 @@ export class HomePage {
 
             newSongRef.set({
               id: newSongRef.key,
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showOptions(songId, songTitle) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Delete Song',
+          role: 'destructive',
+          handler: () => {
+            this.removeSong(songId);
+          }
+        },{
+          text: 'Update title',
+          handler: () => {
+            this.updateSong(songId, songTitle);
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  removeSong(songId: string){
+    this.songsRef.remove(songId);
+  }
+
+  updateSong(songId, songTitle){
+    let prompt = this.alertCtrl.create({
+      title: 'Song Name',
+      message: "Update the name for this song",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title',
+          value: songTitle
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.songsRef.update(songId, {
               title: data.title
             });
           }
